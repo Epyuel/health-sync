b03c9090d34bd882bfc2f35d4e36236c396959c6
Epyuel <epdemrew@gmail.com>
2024-12-07 19:38:09 +0300

    Nextjs project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-row justify-center items-center w-[100%] h-[100vh]">
+      <p className="font-bold">Initial Setup In progress</p>
+    </div>
+  );
+}
